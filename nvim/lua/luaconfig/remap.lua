2bcb27b9b7aea6495da4faee30c6ef39dcdddd98
daniel.low <daniel.low@dyson.com>
2025-02-23 19:33:19 +0800

    feat: intial commit

---
+++ nvim/lua/luaconfig/remap.lua
@@ -0,0 +1,8 @@
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>x", vim.cmd.Ex)  --in normal mode " "+pv = esc
+
+
+-- neotree config
+vim.keymap.set("n", "<leader>e", '<Cmd>Neotree toggle<CR>')
+--vim.keymap.set("n", "<leader>")
---
+++ nvim/lua/plugin/neo-tree.lua
@@ -0,0 +1,12 @@
+require('neo-tree').setup({
+    position = 'float',
+    filesystem = {
+        follow_current_file = true,
+        filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false
+        }
+    },
+})
