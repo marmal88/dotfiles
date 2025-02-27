5afbd5a464753e151a2556513acdff422d4f1310
daniel.low <daniel.low@dyson.com>
2025-02-27 09:42:11 +0800

    feat: added <leader>git as command for neogit

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -1,9 +1,11 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>x", vim.cmd.Ex)  --in normal mode " "+pv = esc
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>x', vim.cmd.Ex)  --in normal mode ' '+pv = esc
 
 -- undotree
 vim.keymap.set('n', '<leader>u', '<Cmd>UndotreeToggle<CR>')
 
 -- neotree config
-vim.keymap.set("n", "<leader>e", '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
 
+-- neogit 
+vim.keymap.set("n", "<leader>git", "<Cmd>Neogit<CR>", { noremap = true, silent = true })
