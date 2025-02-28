4ae7f4cc463ed6dfceaf823b832772c620ebbcb4
daniel.low <daniel.low@dyson.com>
2025-03-01 00:41:12 +0800

    feat: add delete and keep in buffer in visual mode

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>x', vim.cmd.Ex)  --in normal mode ' '+pv = esc
+vim.keymap.set('x', '<leader>p', [["_dP]])    --in visual mode paste and retain register
 
 -- undotree
 vim.keymap.set('n', '<leader>u', '<Cmd>UndotreeToggle<CR>')
@@ -9,3 +10,5 @@ vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
 
 -- neogit 
 vim.keymap.set("n", "<leader>git", "<Cmd>Neogit<CR>", { noremap = true, silent = true })
+
+
