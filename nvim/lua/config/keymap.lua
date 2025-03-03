0a4c09e1d113f01f2980ac831c4719a71ac22667
daniel.low <daniel.low@dyson.com>
2025-03-03 09:34:18 +0800

    feat: added keybindings to allow centering on up and down changes

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -2,6 +2,9 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>x', vim.cmd.Ex)  --in normal mode ' '+pv = esc
 vim.keymap.set('x', '<leader>p', [["_dP]])    --in visual mode paste and retain register
 
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+
 -- undotree
 vim.keymap.set('n', '<leader>u', '<Cmd>UndotreeToggle<CR>')
 
