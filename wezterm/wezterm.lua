baef746667fc01400b988026b0be44317f87b518
daniel.low <daniel.low@dyson.com>
2025-03-05 10:51:21 +0800

    feat: added wezterm configs

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+local c = wezterm.config_builder()
+
+-- font configs
+c.font = wezterm.font {
+  family = 'JetBrains Mono',
+  weight = 'Medium',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- disable ligatures
+}
+c.font_size = 12.0
+c.line_height = 1.0
+
+-- colourscheme configs
+c.color_scheme = 'kanagawa'
+
+-- minimal tabs
+c.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+c.window_background_opacity = 0.96
+c.macos_window_background_blur = 20
+
+return c
