bb8bdba896775d2965b28e2073dcc16eb5440b98
daniel.low <daniel.low@dyson.com>
2025-10-22 00:48:01 +0800

    feat: update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,20 +2,43 @@ local wezterm = require("wezterm")
 local c = wezterm.config_builder()
 
 -- font configs
-c.font = wezterm.font {
-  family = 'JetBrains Mono',
-  weight = 'Medium',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- disable ligatures
-}
-c.font_size = 12.0
-c.line_height = 1.0
+c.font = wezterm.font { family = 'JetBrains Mono' }
+c.font_size = 13.0
+c.line_height = 1.2
 
 -- colourscheme configs
-c.color_scheme = 'kanagawa'
+c.color_scheme = 'kanagawa (Gogh)'
 
 -- minimal tabs
 c.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-c.window_background_opacity = 0.70
+c.window_background_opacity = 0.80
 c.macos_window_background_blur = 20
+c.initial_rows = 40
+c.initial_cols = 180
+
+-- Key bindings
+c.leader = { key = 'x', mods = ' ' }
+c.keys = {
+    {
+        key = 'q',
+        mods = 'LEADER',
+        action = wezterm.action.CloseCurrentPane { confirm = false },
+    },
+    {
+        key = "%",
+        mods = "LEADER",
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain'}
+    },
+    {
+        key = '^',
+        mods = 'LEADER',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain'}
+    },
+    {
+        key = 'k',
+        mods = 'CMD',
+        action = wezterm.action.SendString 'clear\n'
+    }
+}
 
 return c
