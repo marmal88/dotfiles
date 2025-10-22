7b39830a1b5c5416e3df69f9ff88eccc0a504eaa
daniel.low <daniel.low@dyson.com>
2025-10-22 10:58:05 +0800

    feat: update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local c = wezterm.config_builder()
 
 -- font configs
 c.font = wezterm.font { family = 'JetBrains Mono' }
-c.font_size = 13.0
+c.font_size = 12.0
 c.line_height = 1.2
 
 -- colourscheme configs
@@ -11,19 +11,14 @@ c.color_scheme = 'kanagawa (Gogh)'
 
 -- minimal tabs
 c.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-c.window_background_opacity = 0.80
-c.macos_window_background_blur = 20
+c.window_background_opacity = 0.70
+c.macos_window_background_blur = 5
 c.initial_rows = 40
 c.initial_cols = 180
 
 -- Key bindings
-c.leader = { key = 'x', mods = ' ' }
+c.leader = { key = 'b', mods = 'CTRL' }
 c.keys = {
-    {
-        key = 'q',
-        mods = 'LEADER',
-        action = wezterm.action.CloseCurrentPane { confirm = false },
-    },
     {
         key = "%",
         mods = "LEADER",
