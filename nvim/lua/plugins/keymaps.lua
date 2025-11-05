9324dcde06793d24fe4493c50878fef93a7c0584
daniel.low <daniel.low@dyson.com>
2025-11-06 00:36:46 +0800

    feat: adding new keybindings and linter

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -2,34 +2,37 @@
 
 -- Keybindings for plugins
 vim.g.mapleader = " "
-vim.keymap.set( 'n', '<leader>lf', vim.lsp.buf.format )
+vim.keymap.set('n', '<leader>ll', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>w', ':w<CR>')
 
 -- Pickers
-vim.keymap.set( 'n', '<leader>ft', ':Pick cli<CR>', { desc = 'Picker search terminal'} )
-vim.keymap.set( 'n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Fzf-lua search files'} )
-vim.keymap.set( 'n', '<leader>fg', ':FzfLua grep_last<CR>', {desc = 'Fzf-lua search using grep'} )
-vim.keymap.set( 'n', '<leader>fb', ':FzfLua buffers<CR>', {desc = 'Fzf-lua search buffers'} )
-vim.keymap.set( 'n', '<leader>fe', ':FzfLua oldfiles<CR>', {desc = 'Fzf-lua search everything'} )
-vim.keymap.set( 'n', '<leader>fm', ':FzfLua marks<CR>', {desc = 'Fzf-lua search marks'} )
-vim.keymap.set( 'n', '<leader>h', ':FzfLua helptags<CR>', {desc = 'Picker help'} )
+vim.keymap.set('n', '<leader>ft', ':Pick cli<CR>', { desc = 'Picker search terminal' })
+vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Fzf-lua search files' })
+vim.keymap.set('n', '<leader>fg', ':FzfLua grep_last<CR>', { desc = 'Fzf-lua search using grep' })
+vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'Fzf-lua search buffers' })
+vim.keymap.set('n', '<leader>fe', ':FzfLua oldfiles<CR>', { desc = 'Fzf-lua search everything' })
+vim.keymap.set('n', '<leader>fm', ':FzfLua marks<CR>', { desc = 'Fzf-lua search marks' })
+vim.keymap.set('n', '<leader>h', ':FzfLua helptags<CR>', { desc = 'Picker help' })
 
 -- Plugins shortkeys
-vim.keymap.set( 'n', '<leader>e', ':Oil<CR>', {desc = 'File explorer'} )
-vim.keymap.set( 'n', '<leader>g', ':Neogit kind=auto<CR>', {desc = 'Neogit floating'} )
-vim.keymap.set( 'n', '<leader>mp', ':MarkdownPreviewToggle<CR>', {desc = 'Markdown Preview Toggle'})
-vim.keymap.set( 'n', '<leader>u', ':UndotreeToggle<CR>', {desc = 'UndoTree Toggle'})
+vim.keymap.set('n', '<leader>e', ':Oil<CR>', { desc = 'File explorer' })
+vim.keymap.set('n', '<leader>g', ':Neogit kind=auto<CR>', { desc = 'Neogit floating' })
+vim.keymap.set('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', { desc = 'Markdown Preview Toggle' })
+vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>', { desc = 'UndoTree Toggle' })
 
 -- Keybindings (Others)
-vim.keymap.set( 'n', '<C-u>', '<C-u>zz')  -- scroll up and center
-vim.keymap.set( 'n', '<C-d>', '<C-d>zz')  -- scroll down and center
+vim.keymap.set('n', '<C-u>', '<C-u>zz') -- scroll up and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz') -- scroll down and center
 
 -- Window size control
-vim.keymap.set( 'n', '<M-k>', '<cmd>resize +2<CR>')  -- Increase height of pane
-vim.keymap.set( 'n', '<M-j>', '<cmd>resize -2<CR>')  -- Minimize height of pane 
-vim.keymap.set( 'n', '<M-h>', '<cmd>vertical resize +5<CR>')  -- Increase width of pane
-vim.keymap.set( 'n', '<M-l>', '<cmd>vertical resize -5<CR>')  -- Minimize width of pane
+vim.keymap.set('n', '<M-k>', '<cmd>resize +2<CR>')          -- Increase height of pane
+vim.keymap.set('n', '<M-j>', '<cmd>resize -2<CR>')          -- Minimize height of pane
+vim.keymap.set('n', '<M-h>', '<cmd>vertical resize +5<CR>') -- Increase width of pane
+vim.keymap.set('n', '<M-l>', '<cmd>vertical resize -5<CR>') -- Minimize width of pane
 
 -- terminal close
-vim.keymap.set( 'n', '<leader>t', ':botright 10split | terminal<CR>') -- bottom terminal
-vim.keymap.set( 't', '<leader>q', '<C-\\><C-N>')  -- Detach from terminal to Normal mode
+vim.keymap.set('n', '<leader>t', ':botright 10split | terminal<CR>') -- bottom terminal
+vim.keymap.set('t', '<leader>q', '<C-\\><C-N>')                      -- Detach from terminal to Normal mode
 
+-- Coding
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Goto Definition' })
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -3,40 +3,40 @@
 
 -- Installed packages
 vim.pack.add({
-	{ src = 'https://github.com/rebelot/kanagawa.nvim', desc = 'colorscheme'},
-	{ src = 'https://github.com/nvim-tree/nvim-web-devicons', desc = 'icon pack' },
-	{ src = 'https://github.com/ibhagwan/fzf-lua', desc = 'Picker fzf'},
-	{ src = 'https://github.com/nvim-mini/mini.pick', desc = 'Picker mini'},
-	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-	{ src = 'https://github.com/nvim-lualine/lualine.nvim', desc = 'Lua line manager' },
-	{ src = 'https://github.com/akinsho/bufferline.nvim', desc = 'Buffer line manager' },
-	{ src = 'https://github.com/nvim-lua/plenary.nvim', desc = 'async'},
-	{ src = 'https://github.com/mbbill/undotree.git', desc = 'Undo tree'},
-	{ src = 'https://github.com/tpope/vim-surround', desc = 'Surround'},
+	{ src = 'https://github.com/rebelot/kanagawa.nvim',                  desc = 'colorscheme' },
+	{ src = 'https://github.com/nvim-tree/nvim-web-devicons',            desc = 'icon pack' },
+	{ src = 'https://github.com/ibhagwan/fzf-lua',                       desc = 'Picker fzf' },
+	{ src = 'https://github.com/nvim-mini/mini.pick',                    desc = 'Picker mini' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter',        version = 'main' },
+	{ src = 'https://github.com/nvim-lualine/lualine.nvim',              desc = 'Lua line manager' },
+	{ src = 'https://github.com/akinsho/bufferline.nvim',                desc = 'Buffer line manager' },
+	{ src = 'https://github.com/nvim-lua/plenary.nvim',                  desc = 'async' },
+	{ src = 'https://github.com/mbbill/undotree.git',                    desc = 'Undo tree' },
+	{ src = 'https://github.com/tpope/vim-surround',                     desc = 'Surround' },
 	-- Appearence
-	{ src = 'https://github.com/nvim-mini/mini.indentscope', desc = 'Indent'},
-	{ src = 'https://github.com/sphamba/smear-cursor.nvim', desc = 'Cursor Animation'},
-	{ src = 'https://github.com/nvim-mini/mini.notify', desc = 'Notification'},
+	{ src = 'https://github.com/nvim-mini/mini.indentscope',             desc = 'Indent' },
+	{ src = 'https://github.com/sphamba/smear-cursor.nvim',              desc = 'Cursor Animation' },
+	{ src = 'https://github.com/nvim-mini/mini.notify',                  desc = 'Notification' },
 	-- Manage Marks
-	{ src = 'https://github.com/chentoast/marks.nvim', desc = 'Mark Signs'},
+	{ src = 'https://github.com/chentoast/marks.nvim',                   desc = 'Mark Signs' },
 	-- File Explorer
-	{ src = 'https://github.com/stevearc/oil.nvim', desc = 'File Editor'},
+	{ src = 'https://github.com/stevearc/oil.nvim',                      desc = 'File Editor' },
 	-- Language Related Plugins
-	{ src = 'https://github.com/neovim/nvim-lspconfig', desc = 'Language Server Protocol' },
-	{ src = 'https://github.com/mason-org/mason.nvim', desc = 'Language Manager' },
-	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim', desc = 'Language Manager lsp' },
-	{ src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim', desc = 'Diagnostics'},
-	{ src = 'https://github.com/mfussenegger/nvim-lint', desc = 'Linter'},
-	{ src = 'https://github.com/saghen/blink.cmp', desc = 'Autocompletion'},
-	{ src = 'https://github.com/nvim-mini/mini.snippets', desc = 'Snippet Manager'},
+	{ src = 'https://github.com/neovim/nvim-lspconfig',                  desc = 'Language Server Protocol' },
+	{ src = 'https://github.com/mason-org/mason.nvim',                   desc = 'Language Manager' },
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim',         desc = 'Language Manager lsp' },
+	{ src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim', desc = 'Diagnostics' },
+	{ src = 'https://github.com/mfussenegger/nvim-lint',                 desc = 'Linter' },
 	{ src = 'https://github.com/stevearc/conform.nvim',                  desc = 'Formatter' },
+	{ src = 'https://github.com/saghen/blink.cmp',                       desc = 'Autocompletion' },
+	{ src = 'https://github.com/nvim-mini/mini.snippets',                desc = 'Snippet Manager' },
 	-- language specific
-	{ src = 'https://github.com/tmhedberg/simpylfold', desc = 'Python Folding'},
-	{ src = 'https://github.com/iamcco/markdown-preview.nvim', desc = 'Markdown Previewer'},
+	{ src = 'https://github.com/tmhedberg/simpylfold',                   desc = 'Python Folding' },
+	{ src = 'https://github.com/iamcco/markdown-preview.nvim',           desc = 'Markdown Previewer' },
 	-- Git Plugins
-	{ src = 'https://github.com/lewis6991/gitsigns.nvim', desc = 'Git integration for UI'},
-	{ src = 'https://github.com/NeogitOrg/neogit', desc = 'Git workflow'},
-	{ src = 'https://github.com/sindrets/diffview.nvim', desc = 'Git diffview'}
+	{ src = 'https://github.com/lewis6991/gitsigns.nvim',                desc = 'Git integration for UI' },
+	{ src = 'https://github.com/NeogitOrg/neogit',                       desc = 'Git workflow' },
+	{ src = 'https://github.com/sindrets/diffview.nvim',                 desc = 'Git diffview' }
 })
 
 -- Import necessary plugins
@@ -47,12 +47,13 @@ require('plugins.keymaps')
 require('plugins.fzf-lua')
 require('plugins.mini-pick')
 require('plugins.mini-snippets')
-require('mini.indentscope').setup( { symbol = '│' } )
+require('mini.indentscope').setup({ symbol = '│' })
 require('plugins.smear-cursor')
 require('mini.notify').setup()
 -- require('plugins.autocmds')
 require('plugins.blink-cmp')
 require('plugins.marks')
+require('plugins.conform')
 
 -- Colorscheme
 vim.cmd('colorscheme kanagawa')
@@ -66,5 +67,4 @@ require('lsp.ruff')
 require('lsp.pyright')
 require('plugins.diagnostics')
 require('mason').setup()
-vim.lsp.enable({'luals', 'pyright'})
-
+vim.lsp.enable({ 'luals', 'pyright' })
