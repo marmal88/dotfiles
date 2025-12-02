6c652c1b7a5a090d280cbd96651c27dcc5bfbf20
daniel.low <daniel.low@dyson.com>
2025-12-02 16:25:36 +0800

    feat: move fzf search back to original location

--- nvim/lua/plugin/fzf-lua.lua
+++ nvim/lua/plugin/fzf-lua.lua
@@ -1,12 +1,8 @@
-return require('fzf-lua').setup{
+return require('fzf-lua').setup {
 	winopts = {
-		anchor = 'NE',
-		row = 0,
-		col = 0,
 		relative = 'editor',
 		border = 'rounded',
 		backdrop = 80,
-		title_pos = 'left',
 	},
 	preview = {
 		title_pos = 'left',
