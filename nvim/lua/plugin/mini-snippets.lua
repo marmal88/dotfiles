08b03796139f1b10439041f81b33469f624d6064
daniel.low <75230644+marmal88@users.noreply.github.com>
2025-12-08 10:15:32 +0800

    feat: post merge resolve

--- nvim/lua/plugin/mini-snippets.lua
+++
@@ -1,7 +0,0 @@
-local snippets = require('mini.snippets')
-return require('mini.snippets').setup({
-  snippets = {
-    snippets.gen_loader.from_file(vim.fn.stdpath('config') .. '/snippets/global.json'),
-    snippets.gen_loader.from_lang(),
-  },
-})
