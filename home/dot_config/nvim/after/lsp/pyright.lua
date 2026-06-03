ba1576946315add6d8a6212a90faa25a57bba878
Daniel Low <75230644+marmal88@users.noreply.github.com>
2026-06-03 12:41:40 +0800

    feat: remove multiline notifications from lsp (#3)

--- home/dot_config/nvim/after/lsp/pyright.lua
+++ home/dot_config/nvim/after/lsp/pyright.lua
@@ -4,4 +4,14 @@ return {
 	cmd = { "pyright-langserver", "--stdio" },
 	root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
 	filetypes = { "python" },
+	handlers = {
+		--- filter noisy notifications
+		['$/progress'] = function(err, result, ctx)
+			-- just notify once
+			if result.token == (vim.g.basedpyright_progress_token or result.token) then
+				vim.g.basedpyright_progress_token = result.token
+				vim.lsp.handlers['$/progress'](err, result, ctx)
+			end
+		end,
+	},
 }
